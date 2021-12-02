1e639fa07a350dc40d1c67d0f5613f6cd695425a
Jim Challenger <jimchallenger1@gmail.com>
2021-12-02 13:03:26 -0600

    tabs to spaces wups

--- 1/1.rb
+++ 1/1.rb
@@ -1,56 +1,56 @@
 module DayOne
-	def part_one(file)
-		last_measurement = nil
-		increases = 0
+  def part_one(file)
+    last_measurement = nil
+    increases = 0
 
-		File.foreach(file) do |measurement| 
-			measurement = measurement.strip.to_i
-			if last_measurement && measurement > last_measurement
-				increases += 1
-			end
-			
-			last_measurement = measurement
-		end
+    File.foreach(file) do |measurement| 
+      measurement = measurement.strip.to_i
+      if last_measurement && measurement > last_measurement
+        increases += 1
+      end
+      
+      last_measurement = measurement
+    end
 
-		increases
-	end
+    increases
+  end
 
-	def part_two(file)
-		last_window_sum = nil
-		current_window = []
-		increases = 0
+  def part_two(file)
+    last_window_sum = nil
+    current_window = []
+    increases = 0
 
-		File.foreach(file) do |measurement| 
-			measurement = measurement.strip.to_i
-			current_window << measurement
+    File.foreach(file) do |measurement| 
+      measurement = measurement.strip.to_i
+      current_window << measurement
 
-			next if current_window.length < 3
-			current_window.shift if current_window.length > 3
+      next if current_window.length < 3
+      current_window.shift if current_window.length > 3
 
-			if last_window_sum && current_window.sum > last_window_sum
-				increases += 1
-			end
+      if last_window_sum && current_window.sum > last_window_sum
+        increases += 1
+      end
 
-			last_window_sum = current_window.sum
-		end
+      last_window_sum = current_window.sum
+    end
 
-		increases	
-	end
+    increases 
+  end
 
-	def part_one_cons(file)
-		File.readlines(file)
-				.map { |el| el.strip.to_i }
-				.each_cons(2)
-				.map { |pair| pair[1] > pair[0] ? 1 : 0 }
-				.sum
-	end
+  def part_one_cons(file)
+    File.readlines(file)
+        .map { |el| el.strip.to_i }
+        .each_cons(2)
+        .map { |pair| pair[1] > pair[0] ? 1 : 0 }
+        .sum
+  end
 
-	def part_two_cons(file)
-		File.readlines(file)
-				.map { |el| el.strip.to_i }
-				.each_cons(3)
-				.each_cons(2)
-				.map { |windows| windows[1].sum > windows[0].sum ? 1 : 0 }
-				.sum
-	end
+  def part_two_cons(file)
+    File.readlines(file)
+        .map { |el| el.strip.to_i }
+        .each_cons(3)
+        .each_cons(2)
+        .map { |windows| windows[1].sum > windows[0].sum ? 1 : 0 }
+        .sum
+  end
 end
--- 2/2_spec.rb
+++ 2/2_spec.rb
@@ -4,22 +4,22 @@ require './2'
 include DayTwo
 
 RSpec.describe "Day 2" do
-	let(:ex_file) { 'ex_input.txt' }
-	let(:main_file) { 'input.txt' }
+  let(:ex_file) { 'ex_input.txt' }
+  let(:main_file) { 'input.txt' }
 
-	it 'works with the test input for part 1' do
-		expect(part_one(ex_file)).to eq(150)
-	end
+  it 'works with the test input for part 1' do
+    expect(part_one(ex_file)).to eq(150)
+  end
 
-	it 'works with main input for part 1' do
-		expect(part_one(main_file)).to eq(1694130)
-	end
+  it 'works with main input for part 1' do
+    expect(part_one(main_file)).to eq(1694130)
+  end
 
-	it 'works with the test input for part 2' do
-		expect(part_two(ex_file)).to eq(900)
-	end
+  it 'works with the test input for part 2' do
+    expect(part_two(ex_file)).to eq(900)
+  end
 
-	it 'works with main input for part 2' do
-		expect(part_two(main_file)).to eq(1698850445)
-	end
+  it 'works with main input for part 2' do
+    expect(part_two(main_file)).to eq(1698850445)
+  end
 end
