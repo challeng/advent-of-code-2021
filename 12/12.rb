5615942ac95f943ac51ad5c180949cfa0d7410aa
Jim Challenger <jimchallenger1@gmail.com>
2021-12-12 10:54:38 -0600

    Refactor

--- 12/12.rb
+++ 12/12.rb
@@ -1,14 +1,14 @@
 def part_one(file)
-  path_finder = PathFinder1.new(file)
-  path_finder.find_paths
+  path_finder = PathFinder.new(file)
+  path_finder.find_paths_one
 end
 
 def part_two(file)
-  path_finder = PathFinder2.new(file)
-  path_finder.find_paths
+  path_finder = PathFinder.new(file)
+  path_finder.find_paths_two
 end
 
-class PathFinder2
+class PathFinder
   attr_reader :map, :paths
 
   def initialize(file)
@@ -16,7 +16,12 @@ class PathFinder2
     @paths = []
   end
 
-  def find_paths
+  def find_paths_one
+    find('start', ['start'])  
+    paths.count
+  end
+
+  def find_paths_two
     small_caves = map.values.flatten.select do |v|
       v.downcase == v && v != 'start' && v != 'end'
     end.uniq
@@ -24,11 +29,8 @@ class PathFinder2
     paths.uniq.count
   end
 
-  def find(node, path, cave)
-    if node == 'end'
-      paths << path
-      return
-    end
+  def find(node, path, cave=nil)
+    paths << path and return if node == 'end'
 
     map[node].each do |dest|
       next if cannot_visit?(dest, path, cave)
@@ -39,53 +41,8 @@ class PathFinder2
   def cannot_visit?(dest, path, cave)
     dest == 'start' ||
       path.last == 'end' ||
-      (small_cave?(dest) && dest != cave && path.include?(dest)) ||
-      (small_cave?(dest) && dest == cave && path.count{|node| node == dest} == 2)
-  end
-
-  def small_cave?(dest)
-    dest == dest.downcase 
-  end
-
-  def parse_file(file)
-    @map = {}
-    File.readlines(file, chomp: true).each do |line|
-      start, finish = line.split('-') 
-      @map[start] ||= []
-      @map[start] << finish
-      @map[finish] ||= []
-      @map[finish] << start
-    end
-  end
-end
-
-class PathFinder1
-  attr_reader :map, :paths
-
-  def initialize(file)
-    parse_file(file)
-    @paths = []
-  end
-
-  def find_paths
-    find('start', ['start'])  
-    paths.count
-  end
-
-  def find(node, path)
-    if node == 'end'
-      paths << path
-      return
-    end
-
-    map[node].each do |dest|
-      next if cannot_visit?(dest, path)
-      find(dest, path + [dest])
-    end
-  end
-
-  def cannot_visit?(dest, path)
-    (path.include?(dest) && small_cave?(dest)) || path.last == 'end'
+      (dest != cave && small_cave?(dest) && path.include?(dest)) ||
+      (dest == cave && small_cave?(dest) && path.count{|node| node == dest} == 2)
   end
 
   def small_cave?(dest)
